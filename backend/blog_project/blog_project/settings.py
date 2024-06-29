aeeedf338b6199708927bd39b50942f2af77c856
felipetodev <fe.ossandon.u@gmail.com>
2024-06-29 02:13:15 -0400

    fix(backend): allow front dev routes

--- backend/blog_project/blog_project/settings.py
+++ backend/blog_project/blog_project/settings.py
@@ -128,7 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:8000',
+    'http://127.0.0.1:3000',
+    'http://localhost:3000',
 ]
 
 # REST framework settings
