e5f2fa0ed80177fa14fab6b46a35d990c1bdcf6e
felipetodev <fe.ossandon.u@gmail.com>
2024-06-29 18:09:18 -0400

    feat: add toast and next themes

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import { Header } from "@/components/header";
+import { Providers } from "@/components/providers";
 import { cn } from "@/lib/utils";
 import "./globals.css";
 
@@ -14,23 +15,37 @@ export const metadata: Metadata = {
   description: "A blog app built with Django and Next.js",
 };
 
+export const viewport = {
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' }
+  ]
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html className="dark" lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           "min-h-screen font-sans antialiased bg-gradient-to-b from-background from-30% to-100% to-primary/10",
           fontSans.variable
         )}
       >
-        <Header />
-        <main className="flex flex-col w-full pt-[60px] h-screen px-8 md:px-14 lg:px-28 overflow-y-auto">
-          {children}
-        </main>
+        <Providers
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main className="flex flex-col w-full pt-[60px] h-screen px-8 md:px-14 lg:px-28 overflow-y-auto">
+            {children}
+          </main>
+        </Providers>
       </body>
     </html>
   );
