ddf7c45cd8ca91cf42f4db03f248bfba46b79452
felipetodev <fe.ossandon.u@gmail.com>
2024-06-28 23:33:09 -0400

    feat: add init front project

---
+++ frontend/app/[...posts]/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1 className="text-white text-4xl">
+        Hello Blog!
+      </h1>
+    </main>
+  );
+}
