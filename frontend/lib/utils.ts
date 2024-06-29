5413a9c42cb5943e159d5bfee8efd85429babc7b
felipetodev <fe.ossandon.u@gmail.com>
2024-06-29 02:26:31 -0400

    feat: add basic pagination in blog

--- frontend/lib/utils.ts
+++ frontend/lib/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getPageParams(url: string): string {
+  const urlParams = url.split("?").at(-1);
+  const params = new URLSearchParams(urlParams);
+  return params.get("page")!;
+}
