584aaf9323698c5d399c9536620dc3f3030b182a
felipetodev <fe.ossandon.u@gmail.com>
2024-06-30 16:13:52 -0400

    fix: refact icons

--- frontend/app/actions/submit-button.tsx
+++ frontend/app/actions/submit-button.tsx
@@ -2,31 +2,7 @@
 
 import { useFormStatus } from 'react-dom'
 import { Button } from '@/components/ui/button'
-
-function Spinner(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      {...props}
-    >
-      <path d="M12 2v4" />
-      <path d="m16.2 7.8 2.9-2.9" />
-      <path d="M18 12h4" />
-      <path d="m16.2 16.2 2.9 2.9" />
-      <path d="M12 18v4" />
-      <path d="m4.9 19.1 2.9-2.9" />
-      <path d="M2 12h4" />
-      <path d="m4.9 4.9 2.9 2.9" />
-    </svg>
-  )
-}
+import { Spinner } from '@/components/ui/icons'
 
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>
 
